980a3288c02df5a79bd6db2f1f7973a5ff0338cf
hernan <hernan.pineros@ilumno.com>
2022-04-05 23:59:35 -0500

    create routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/home";
+import DetailsProduct from "./pages/details-product";
 
 function App() {
-  return (
-    <div className="container">
-
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/items" element={<DetailsProduct/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/details-product.js
@@ -0,0 +1,9 @@
+
+function DetailsProduct() {
+  return (
+      <div className="container">
+          Detail
+      </div>
+  );
+}
+export default DetailsProduct;
